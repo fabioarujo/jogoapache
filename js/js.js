99a0d8d7b60be8e4f0d2dae7e824e11070e96f56
Fábio Araújo <fabioaraujo1602@gmail.com>
2022-03-10 21:27:27 -0300

    Criando movimentacao

--- js/js.js
+++ js/js.js
@@ -9,6 +9,26 @@ function start() {
 
 //Principais variáveis do jogo
 var jogo = {}
+var velocidade=5;
+var posicaoY = parseInt(Math.random() * 334);
+var TECLA = {
+	W: 87,
+	S: 83,
+	D: 68
+	}
+
+	jogo.pressionou = [];
+
+//Verifica se o usuário pressionou alguma tecla	
+	
+$(document).keydown(function(e){
+	jogo.pressionou[e.which] = true;
+	});
+
+
+	$(document).keyup(function(e){
+       jogo.pressionou[e.which] = false;
+	});
 	
 //Game Loop
 
@@ -17,6 +37,8 @@ jogo.timer = setInterval(loop,30);
 function loop() {
 
 movefundo();
+movejogador();
+moveinimigo1();
 
 } // Fim da função loop()
 
@@ -28,3 +50,48 @@ function movefundo() {
 	$("#fundoGame").css("background-position",esquerda-1);
 	
 } // fim da função movefundo()
+
+function movejogador() {
+	
+	if (jogo.pressionou[TECLA.W]) {
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo-10);
+
+        if (topo<=0) {
+		
+            $("#jogador").css("top",topo+10);
+        }
+	
+	}
+	
+	if (jogo.pressionou[TECLA.S]) {
+		
+		var topo = parseInt($("#jogador").css("top"));
+		$("#jogador").css("top",topo+10);	
+
+        if (topo>=434) {	
+            $("#jogador").css("top",topo-10);
+                
+        }
+        
+	}
+	
+	if (jogo.pressionou[TECLA.D]) {
+		
+		//Chama função Disparo	
+	}
+}
+
+function moveinimigo1() {
+
+	posicaoX = parseInt($("#inimigo1").css("left"));
+	$("#inimigo1").css("left",posicaoX-velocidade);
+	$("#inimigo1").css("top",posicaoY);
+		
+		if (posicaoX<=0) {
+		posicaoY = parseInt(Math.random() * 334);
+		$("#inimigo1").css("left",694);
+		$("#inimigo1").css("top",posicaoY);
+			
+		}
+} //Fim da função moveinimigo1()
